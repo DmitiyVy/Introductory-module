232122a3754b9e7ceaf720e90cf126e33a1eab91
DmitiyVy <dmitriy.jons@gmail.com>
2024-11-13 22:07:03 +0700

    variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+cource_name = "Python"
+time_for_one_tasks = number_of_hours_spent / number_of_completed_tasks
+print(f"Название курса: {cource_name}, Количество выполненных ДЗ: {number_of_completed_tasks}, Количество затраченных часов: {number_of_hours_spent}, Время на одно задание: {time_for_one_tasks}.")
+# Поправил код с помощью f-строки, после замечаения к практическому заданию (по поводу названия переменных) и изучения доп.информации от преподавателя
